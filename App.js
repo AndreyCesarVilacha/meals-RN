e5562b6908cdf2e5a226f677e2372187ac0da522
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-08-23 13:38:13 -0300

    Using fonts

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+
+//expo install expo-app-loading
+import AppLoading from 'expo-app-loading';
+
+//Also add the following prop to the <AppLoading /> component (in your JSX code):
+//onError={(err) => console.log(err)}
+
+const fetchFonts = () => {
+    return Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    })
+}
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded){
+    return (
+      <AppLoading 
+        startAsync={fetchFonts}
+        onFinish={()=> setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
