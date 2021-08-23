2d509b6ff0c152e54a730fdeacbf93ab6cc04240
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-08-23 16:01:00 -0300

    Testing navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,7 +3,6 @@ import React from 'react';
 //yarn add @react-navigation/stack
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//yarn add @react-navigation/drawer
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
@@ -15,9 +14,9 @@ function MealsNavigator(){
     return(
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Categories Screen" component={CategoriesScreen} />
-                <Stack.Screen name="Category Meals Screen" component={CategoryMealsScreen} />
-                <Stack.Screen name="Meal Detail Screen" component={MealDetailScreen} />
+                <Stack.Screen name="Categories" component={CategoriesScreen} />
+                <Stack.Screen name="Category Meals" component={CategoryMealsScreen} />
+                <Stack.Screen name="Meal Detail" component={MealDetailScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
